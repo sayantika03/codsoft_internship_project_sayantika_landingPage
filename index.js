f6072dbe6cda627d21f15a64543538dc52c891a7
sayantika03 <104552568+sayantika03@users.noreply.github.com>
2023-08-28 20:01:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    reset: true,
+    distance: '90px',
+    duration: 1500,
+    delay: 200
+});
+
+ScrollReveal().reveal('.second',{interval:100,origin:'left'});
+ScrollReveal().reveal('.fifth',{interval:100,origin:'bottom'});
+ScrollReveal().reveal('.col',{scale:0.1});
+ScrollReveal().reveal('.fifth',{interval:100,origin:'left'});
+ScrollReveal().reveal('.bye',{interval:100,origin:'bottom'});
+ScrollReveal().reveal('.card-text',{interval:100,origin:'left'});
+ScrollReveal().reveal('.fa',{interval:100,origin:'left'});
